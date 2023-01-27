8513333c2d4226b91ce71f2767a7d77ac381aa2a
karthikgoud <karthikrv09@gmail.com>
2023-01-27 12:05:53 +0530

    using this.props

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,12 +5,12 @@ class Body extends Component {
   render() {
     return (
       <div className="card-body">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card name="restaurant" location="ballari" />
+        <Card name="restaurant" location="ballari" />
+        <Card name="cart" location="mysore" />
+        <Card name="restaurant" location="ballari" />
+        <Card name="taxi" location="hubli" />
+        <Card name="restaurant" location="ballari" />
       </div>
     );
   }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,18 @@
-const Card = () => {
-  return (
-    <div className="card">
-      <h1>Restaurant cards</h1>
-    </div>
-  );
-};
+import { Component } from "react";
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="card">
+        <h1>Restaurant cards</h1>
+        <h2>{this.props.name}</h2>
+        <h2>{this.props.location}</h2>
+      </div>
+    );
+  }
+}
 
 export default Card;
